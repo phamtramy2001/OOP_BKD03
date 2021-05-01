41ab2da13bc77e004e15cf638f4a274175d149f5
phamtramy2001 <76618225+phamtramy2001@users.noreply.github.com>
2021-05-01 10:29:03 +0700

    baitap

--- src/unit1/student.java
+++ src/unit1/student.java
@@ -1,10 +1,9 @@
 package unit1;
-import java.io.Console;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 public class student {
 	public static void main(String[] args) {
-        double diemKiemTraHS1, diemKiemTraHS2, diemThi, diemTrungBinh;
+        double diemKiemTraC, diemKiemTraP, diemTrungBinh;
         String ketQuaXepLoai;   
          
         Scanner scanner = new Scanner(System.in);  
@@ -12,25 +11,25 @@ public class student {
         DecimalFormat decimalFormat = new DecimalFormat("#.#");
         
         System.out.println("Nhập vào điểm kiểm tra C:");
-        diemKiemTraHS1 = scanner.nextDouble();
+        diemKiemTraC = scanner.nextDouble();
         System.out.println("Nhập vào điểm kiểm tra P: ");
-        diemKiemTraHS2 = scanner.nextDouble();
+        diemKiemTraP = scanner.nextDouble();
         
         
-        if ((diemKiemTraHS1 < 0 || diemKiemTraHS1 > 10) 
-            || (diemKiemTraHS2 < 0 || diemKiemTraHS2 > 10)) {
+        if ((diemKiemTraC < 0 || diemKiemTraC > 10) 
+            || (diemKiemTraP < 0 || diemKiemTraP > 10)) {
             System.out.println("Nhập điểm không hợp lệ.");
         } else {
          
             
-            diemTrungBinh = ((diemKiemTraHS1 + diemKiemTraHS2 ) /2);
+            diemTrungBinh = ((diemKiemTraC + diemKiemTraP ) /2);
             decimalFormat.format(diemTrungBinh);
              
             
             if (diemTrungBinh >= 8) {
                 ketQuaXepLoai = "Giỏi";
             } else if ((diemTrungBinh >=6 ) && (diemTrungBinh < 8)) {
-                // toán tử AND: đúng khi cả hai biểu thức đều đúng
+                
                 ketQuaXepLoai = "Khá";
             } else if ((diemTrungBinh >= 5) && (diemTrungBinh < 6)) {
                 ketQuaXepLoai = "Trung Bình";
