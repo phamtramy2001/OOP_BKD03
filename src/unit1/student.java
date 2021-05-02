e36f28ed0a6041865b5e39be30abe6ecfb048356
phamtramy2001 <76618225+phamtramy2001@users.noreply.github.com>
2021-05-02 17:45:08 +0700

    Update student.java

--- src/unit1/student.java
+++ src/unit1/student.java
@@ -3,13 +3,17 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 public class student {
 	public static void main(String[] args) {
+	String name, claass;
         double diemKiemTraC, diemKiemTraP, diemTrungBinh;
         String ketQuaXepLoai;   
          
         Scanner scanner = new Scanner(System.in);  
         
         DecimalFormat decimalFormat = new DecimalFormat("#.#");
-        
+        System.out.println("Nhập vào tên sv:");
+        name = scanner.nextLine();
+        System.out.println("Nhập vào tên lớp:");
+        claass = scanner.nextLine();
         System.out.println("Nhập vào điểm kiểm tra C:");
         diemKiemTraC = scanner.nextDouble();
         System.out.println("Nhập vào điểm kiểm tra P: ");
@@ -36,7 +40,7 @@ public class student {
             }  else {
                 ketQuaXepLoai = "Yếu";
             }
-            System.out.println("Kết quả xếp loại môn học của sinh viên là " + ketQuaXepLoai);
+            System.out.println("Kết quả xếp loại môn học của sinh viên :  : "+name+" Lớp " + claass+" là " + ketQuaXepLoai);
         }
      
     }
